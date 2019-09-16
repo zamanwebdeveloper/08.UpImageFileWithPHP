2a17444159c33d1e8e9a648fea33108e89ddb23b
Syed Zaman Mostafiz <zamanwebdeveloper>
2019-09-17 00:42:21 +0600

    08.UpImageFileWithPHP

---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+</section>
+
+		<section class="footeroption">
+			<h2>Website: <a href="https://zamanwebdeveloper.com/" target="_blank">zamanwebdeveloper.com</a></h2>
+		</section>
+	</div>
+
+</body>
+</html>
